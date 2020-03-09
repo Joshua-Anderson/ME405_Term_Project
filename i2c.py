6c9b7b7e45d9ab01351d379ba0ceaa8f0ba7b53f
Joshua Anderson <j@joshua-anderson.com>
2020-03-08 18:16:12 -0700

    Added better documentation

--- i2c.py
+++ i2c.py
@@ -1,6 +1,20 @@
+# -*- coding: utf-8 -*-
+
+##
+# @file i2c.py
+# @author Josh Anderson
+# @author Ethan Czuppa
+#
+# Contains I2C buses for the SUMO bot
+
 from machine import I2C
 import pyb
 
+## I2C Bus 1. Contains left time of flight sensor and accelerometer.
 Bus1 = I2C(1)
+
+## I2C Bus 2. Contains center time of flight sensor.
 Bus2 = I2C(-1, pyb.Pin.board.PC3, pyb.Pin.board.PC2)
+
+## I2C Bus 3. Contains center time of flight sensor.
 Bus3 = I2C(-1, pyb.Pin.board.PC0, pyb.Pin.board.PB0)
--- tof.py
+++ tof.py
@@ -1,15 +1,35 @@
+# -*- coding: utf-8 -*-
+
+##
+# @file tof.py
+# @author Josh Anderson
+# @author Ethan Czuppa
+#
+# Contains time of flight sensor code and sensor declaritions.
+
 import VL53L0X
 import i2c
 
 
 class TOF:
+    """ Time of Flight Sensor Class """
+
     def __init__(self, i2c):
+        """ Initialize and start a TOF sensor on an i2c bus
+        @param i2c Initialized I2C bus class
+        """
         self.sensor = VL53L0X.VL53L0X(i2c)
         self.sensor.start()
 
     def read(self):
+        """ Read distance in mm from sensors """
         return self.sensor.read()
 
+## Left Time of Flight Sensor
 Left = TOF(i2c.Bus1)
+
+## Center Time of Flight Sensor
 Center = TOF(i2c.Bus2)
+
+## Right Time of Flight Sensor
 Right = TOF(i2c.Bus3)
